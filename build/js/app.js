20eadec59a9b0697248153790504296093e7311b
Robert-Thaiyah <bobthaiya@gmail.com>
2023-05-30 13:50:43 +0300

    code cleanup

--- build/js/app.js
+++ build/js/app.js
@@ -6,38 +6,6 @@ btn.addEventListener('click', ()=>{
     nav.classList.toggle('hidden')
 })
 
-// typewriter effect
-const words = ["Hello!", "Bonjour!", "Shikamoo!","Shalom!"];
-let i = 0;
-let j = 0;
-let currentWord = "";
-let isDeleting = false;
-
-function type() {
-  currentWord = words[i];
-  if (isDeleting) {
-    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
-    j--;
-    if (j == 0) {
-      isDeleting = false;
-      i++;
-      if (i == words.length) {
-        i = 0;
-      }
-    }
-  } else {
-    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
-    j++;
-    if (j == currentWord.length) {
-      isDeleting = true;
-    }
-  }
-  setTimeout(type, 200);
-}
-
-type();
-
-// end of typewriter
 
 // date at footer
 const date = new Date()
